72fc8ce6f7848fb588df48577346db80b1b992ac
Pavikkaran <142660254+Pavikkaran-p@users.noreply.github.com>
2024-06-13 09:02:54 +0530

    url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:5500/api',
+        target:'https://creatorhire.onrender.com/api',
         changeOrigin:true,
         rewrite: path => path.replace(/^\/api/, '')
       }
